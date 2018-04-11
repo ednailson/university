11ded09d7b5660a9353cc348fd6b031d32b3ae98
Júnior Vilas Boas <evbcjr@gmail.com>
2018-04-11 18:37:14 +0200

    refactoring

--- main.c
+++ main.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 void printVector(int v[], int size) {
     printf("VETOR: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", v[i]);
-    }
+    for (int i = 0; i < size; i++) printf("%d ", v[i]);
     printf("\n");
 }
 
@@ -23,16 +20,14 @@ void vectorOrder(int v[], int size) {
         if (changed > 0) {
             printVector(v, size);
             changed = 0;
-        } else {
-            break;
-        }
+        } else break;
     } while (1);
 }
 
 int main() {
     printf("# ANALISE DE ALGORITIMO # \n\n- Metódo bolha para ordenação -\n\n");
-    int vector[5] = {2, 6, 4, 5, 1};
-    vectorOrder(vector, 5);
+    int vector[9] = {2, 6, 4, 5, 1, 20, 6, -4, 29};
+    vectorOrder(vector, 9);
     printf("\n##  FIM  ##");
     return 0;
 }

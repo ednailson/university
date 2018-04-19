84836dcc160e53322b0699510e0f8183f1628458
Júnior Vilas Boas <evbcjr@gmail.com>
2018-04-19 18:36:44 +0200

    bubblesort homework

---
+++ cmake-build-debug/CMakeFiles/university_activities.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/university_activities.dir/main.c.o"
+  "university_activities.pdb"
+  "university_activities"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/university_activities.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++ main.c
@@ -1,32 +1,48 @@
 #include <stdio.h>
 
-void printVector(int v[], int size) {
-    printf("VETOR: ");
-    for (int i = 0; i < size; i++) printf("%d ", v[i]);
+void printVector(int v[], int size, int position1, int position2, int changed) {
+    if (changed == -1) printf("\n");
+    else if (changed == -2) printf("\nVETOR FINAL DESSA INTERAÇÃO: ");
+    else if (changed == -3) printf("\n## VETOR ORDENADO ##\n");
+    else printf("ORDENAÇÃO %d: ", changed);
+    for (int i = 0; i < size; i++){
+        if (i==position1) printf("[");
+        printf(" %d ", v[i]);
+        if (i==position2) printf("]");
+    }
     printf("\n");
 }
 
 void vectorOrder(int v[], int size) {
     int changed = 0;
+    int control = 0;
     do {
         for (int i = 0; i < size - 1; i++) {
             if (v[i] > v[i + 1]) {
                 int saveValue = v[i];
                 v[i] = v[i + 1];
                 v[i + 1] = saveValue;
-                changed++;
+                control++;
+                printVector(v,size, i, i+1, changed+1);
             }
         }
-        if (changed > 0) {
-            printVector(v, size);
-            changed = 0;
+        if (control > 0) {
+            changed++;
+            printVector(v, size, -1, -1, -2);
+            printf("\n");
+            control = 0;
         } else break;
     } while (1);
+    printVector(v, size, -1, -1, -3);
 }
 
 int main() {
-    printf("# ANALISE DE ALGORITIMO # \n\n- Metódo bolha para ordenação -\n\n");
-    int vector[9] = {2, 6, 4, 5, 1, 20, 6, -4, 29};
+    printf("# ANALISE DE ALGORITIMO # \n\n- Metódo bolha para ordenação -\n");
+    printf("\n\n-- VETOR --");
+    int vector[9] = {2, 6, 4, 7, 1, 20, 6, -4, 29};
+    printVector(vector, 9, -1, -1, -1);
+    printf("\n");
+    printf("\n@@ ORDENANDO @@\n\n");
     vectorOrder(vector, 9);
     printf("\n##  FIM  ##");
     return 0;

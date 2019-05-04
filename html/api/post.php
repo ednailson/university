0207a291948705fbbd0b94fa50e0478e22d0a8e8
ednailson <evbcjr@gmail.com>
2019-05-04 20:26:13 -0300

    post relation

--- html/api/post.php
+++ html/api/post.php
@@ -16,7 +16,25 @@ try{
                     $data=array("status"=>"400","message"=>"Error creating post");
                     break;
                 }
-                $data=array("status"=>"200","data"=>"Successful post registration");
+                $result = $conn->query("SELECT post_id FROM posts WHERE fk_user_id = \"$u\" AND text = \"$p\" AND images = \"$i\" ");
+                if (!$result) {
+                    $data=array("status"=>"400","message"=>"Error creating post");
+                    break;
+                }
+                $post_id = $result->fetch_all()[0][0];
+                $result2 = $conn->query("SELECT users.user_id FROM friendship AS f INNER JOIN users WHERE fk_user_1 = \"$u\" AND f.fk_user_2 = users.user_id OR fk_user_2 = \"$u\" AND f.fk_user_1 = users.user_id");
+                if (!$result2) {
+                    $data=array("status"=>"400","message"=>"Error getting friendship");
+                    break;
+                }
+                foreach ($result2->fetch_all() as $key => $value) {
+                    $result3 = $conn->query("INSERT INTO friendship_posts (fk_post_id, fk_user_id) VALUES (\"$post_id\", \"$value[0]\")");
+                    if (!$result3) {
+                        $data=array("status"=>"400","message"=>"Error relation post and user");
+                        break;
+                    }
+                }
+                $data=array("status"=>"200","data"=>$post_id);
                 break;
             case 'GET':
                 $u = $_REQUEST['user_id'];
